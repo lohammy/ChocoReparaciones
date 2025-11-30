712afaabe6eebdfd1b38e81ae9a43940538dd770
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-30 01:01:20 -0400

    migracion

--- ChocoReparaciones/Data/Migrations/20251130044949_Modelos.Designer.cs
+++ ChocoReparaciones/Data/Migrations/20251130044949_Modelos.Designer.cs
@@ -4,6 +4,7 @@ using ChocoReparaciones.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChocoReparaciones.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251130044949_Modelos")]
+    partial class Modelos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
