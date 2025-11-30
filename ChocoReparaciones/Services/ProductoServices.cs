345ea771ddba00ec538164d5b1d6c8794c72f6f4
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-30 02:25:07 -0400

    cree el servicio de productos

---
+++ ChocoReparaciones/Services/ProductoServices.cs
@@ -0,0 +1,56 @@
+﻿using ChocoReparaciones.Data;
+using ChocoReparaciones.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChocoReparaciones.Services;
+
+public class ProductoServices
+{
+	private readonly ApplicationDbContext _context;
+
+	public ProductoServices(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<List<Producto>> ObtenerTodos()
+	{
+		return await _context.Productos
+			.OrderByDescending(p => p.Id)
+			.ToListAsync();
+	}
+	public async Task<List<Producto>> ObtenerProductos()
+	{
+		return await _context.Productos
+			.Where(p => p.CantidadDisponible > 0)
+			.OrderByDescending(p => p.Id)
+			.ToListAsync();
+	}
+	public async Task AgregarProducto(Producto producto)
+	{
+		_context.Productos.Add(producto);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task<Producto?> ObtenerPorId(int id)
+	{
+		return await _context.Productos.FindAsync(id);
+	}
+
+	public async Task EliminarProducto(int id)
+	{
+		var producto = await _context.Productos.FindAsync(id);
+		if (producto != null)
+		{
+			_context.Productos.Remove(producto);
+			await _context.SaveChangesAsync();
+		}
+	}
+
+	public async Task ActualizarProducto(Producto producto)
+	{
+		_context.Productos.Update(producto);
+		await _context.SaveChangesAsync();
+	}
+}
+
