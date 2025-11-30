8935c99cef6f8c9aa4fff1154346873b0037e635
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-30 01:01:38 -0400

    agregue los modelos

--- ChocoReparaciones/Data/ApplicationDbContext.cs
+++ ChocoReparaciones/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
+using ChocoReparaciones.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ChocoReparaciones.Data
-{
+namespace ChocoReparaciones.Data;
+
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-    }
+	public DbSet<Cliente> Clientes { get; set; }
+	public DbSet<Producto> Productos { get; set; }
+	public DbSet<Reparacion> Reparaciones { get; set; }
+	public DbSet<Usuario> Usuarios { get; set; }
 }
