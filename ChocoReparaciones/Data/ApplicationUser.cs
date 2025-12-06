91cd8e4f0ced88e677abbf2c5b837ff6d15780bc
lohammy vasquez adames <2005lohammy@gmail.com>
2025-12-05 20:45:02 -0400

    feat: agrega datos necesarios para usar el login/register

--- ChocoReparaciones/Data/ApplicationUser.cs
+++ ChocoReparaciones/Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace ChocoReparaciones.Data
@@ -5,6 +6,17 @@ namespace ChocoReparaciones.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-    }
+		[Required]
+		[MaxLength(100)]
+		public string NombreCompleto { get; set; } = "";
+
+		[Required]
+		[MaxLength(250)]
+		public string Direccion { get; set; } = "";
+
+		[Required]
+		[Phone]
+		public string Telefono { get; set; } = "";
+	}
 
 }
