53f63adfcaf3c3d2c8fc0267aaf4c117027429bf
lohammy vasquez adames <2005lohammy@gmail.com>
2025-12-06 16:52:44 -0400

    refactor: elimina inyeccion innecesaria

--- ChocoReparaciones/Data/ApplicationDbContext.cs
+++ ChocoReparaciones/Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace ChocoReparaciones.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-	public DbSet<Cliente> Clientes { get; set; }
 	public DbSet<Producto> Productos { get; set; }
 	public DbSet<Reparacion> Reparaciones { get; set; }
 	public DbSet<Usuario> Usuarios { get; set; }
