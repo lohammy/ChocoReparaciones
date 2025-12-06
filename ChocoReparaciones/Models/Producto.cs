77efdedb41204cc809d040f02e92245d8909636b
lohammy vasquez adames <2005lohammy@gmail.com>
2025-12-06 17:01:32 -0400

    feat: crea la tabla ReparacionDetalle y agrega las relaciones

--- ChocoReparaciones/Models/Producto.cs
+++ ChocoReparaciones/Models/Producto.cs
@@ -3,17 +3,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ChocoReparaciones.Models
 {
-    public class Producto
-    {
+	public class Producto
+	{
 		public int Id { get; set; }
 
 		[Required(ErrorMessage = "El nombre del producto es obligatorio.")]
 		[StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
-		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras.")]
 		public string? Nombre { get; set; }
 
 		[Required(ErrorMessage = "La descripción es obligatoria.")]
-		[StringLength(30, ErrorMessage = "La descripción no puede superar los 30 caracteres.")]
+		[StringLength(300, ErrorMessage = "La descripción no puede superar los 300 caracteres.")]
 		public string? Descripcion { get; set; }
 
 		[Required(ErrorMessage = "La categoría es obligatoria.")]
@@ -30,5 +30,7 @@ namespace ChocoReparaciones.Models
 		[NotMapped]
 		[Range(1, int.MaxValue, ErrorMessage = "Debe ingresar una cantidad válida para agregar stock.")]
 		public int StockExtra { get; set; } = 1;
+
+		public virtual ICollection<ReparacionDetalle> ReparacionDetalles { get; set; } = new List<ReparacionDetalle>();
 	}
 }
--- ChocoReparaciones/Models/Reparacion.cs
+++ ChocoReparaciones/Models/Reparacion.cs
@@ -2,20 +2,25 @@
 
 namespace ChocoReparaciones.Models
 {
-    public class Reparacion
-    {
+	public class Reparacion
+	{
 		public int Id { get; set; }
 
 		[Required]
-		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras.")]
 		public string ClienteNombre { get; set; } = string.Empty;
 
+		[Required(ErrorMessage = "El número de teléfono es obligatorio.")]
+		[Phone(ErrorMessage = "El número de teléfono no es válido.")]
+		[Range(minimum: 9, maximum: 9, ErrorMessage = "Debe introducir un numero valido")]
+		public string Telefono { get; set; } = "";
+
 		[Required]
-		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras.")]
 		public string DescripcionEquipo { get; set; } = string.Empty;
 
 		[Required]
-		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+		[RegularExpression(@"^[a-zA-Z\\s]+$", ErrorMessage = "En este campo solo se permiten letras.")]
 		public string ProblemaReportado { get; set; } = string.Empty;
 
 		public DateTime FechaIngreso { get; set; } = DateTime.Now;
@@ -23,6 +28,7 @@ namespace ChocoReparaciones.Models
 		public string Estado { get; set; } = "Pendiente";
 
 		public string? TecnicoAsignadoId { get; set; }
+
 		public string? ComentarioTecnico { get; set; }
 
 		[Required(ErrorMessage = "Debe seleccionar una prioridad.")]
@@ -30,5 +36,8 @@ namespace ChocoReparaciones.Models
 
 		[Required]
 		public DateTime FechaEntrega { get; set; } = DateTime.Today.AddDays(3);
+
+		// Navigation Property - Colección de detalles
+		public virtual ICollection<ReparacionDetalle> ReparacionDetalles { get; set; } = new List<ReparacionDetalle>();
 	}
 }
