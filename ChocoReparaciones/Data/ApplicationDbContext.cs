faab529130eeb117c455b20a62dfd46d80624fdf
lohammy vasquez adames <2005lohammy@gmail.com>
2025-12-07 19:13:53 -0400

    feat: agrega relaciones en el contexto

--- ChocoReparaciones/Data/ApplicationDbContext.cs
+++ ChocoReparaciones/Data/ApplicationDbContext.cs
@@ -8,5 +8,23 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
 	public DbSet<Producto> Productos { get; set; }
 	public DbSet<Reparacion> Reparaciones { get; set; }
+	public DbSet<ReparacionDetalle> ReparacionDetalles { get; set; } 
 	public DbSet<Usuario> Usuarios { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<ReparacionDetalle>()
+			.HasOne(d => d.Reparacion)
+			.WithMany(r => r.ReparacionDetalles)
+			.HasForeignKey(d => d.ReparacionId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		modelBuilder.Entity<ReparacionDetalle>()
+			.HasOne(d => d.Producto)
+			.WithMany(p => p.ReparacionDetalles)
+			.HasForeignKey(d => d.ProductoId)
+			.OnDelete(DeleteBehavior.Restrict);
+	}
 }
